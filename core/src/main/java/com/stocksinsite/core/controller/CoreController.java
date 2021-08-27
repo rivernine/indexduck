a3aa56126f4c3d554e1da56916201a8ab9d1c639
Jack <rkdworn9411@gmail.com>
2021-08-27 23:20:15 +0900

    add: websocket

--- core/src/main/java/com/stocksinsite/core/controller/CoreController.java
+++ core/src/main/java/com/stocksinsite/core/controller/CoreController.java
@@ -1,7 +1,10 @@
 package com.stocksinsite.core.controller;
 
+import com.stocksinsite.core.controller.dto.TextMessageDTO;
 import com.stocksinsite.core.service.CoreService;
 
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,4 +21,8 @@ public class CoreController {
     return coreService.print_hello();
   }
 
+  @SendTo("/topic/message")
+	public TextMessageDTO broadcastMessage(@Payload TextMessageDTO textMessageDTO) {
+		return textMessageDTO;
+	}
 }
---
+++ core/src/main/java/com/stocksinsite/core/controller/dto/TextMessageDTO.java
@@ -0,0 +1,14 @@
+package com.stocksinsite.core.controller.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@Setter
+public class TextMessageDTO {
+
+	private String message;
+
+}
