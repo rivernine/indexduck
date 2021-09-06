e527c53a11025c8102582464d7649f49ed8ef93c
rivernine <rivernine@github.com>
2021-09-06 14:40:24 +0900

    update

--- pykrx/INDEX_DUCK.sql
+++ pykrx/INDEX_DUCK.sql
@@ -40,6 +40,31 @@ CREATE TABLE last_3m  (
   primary key (ticker, date)
 ) engine = InnoDB;
 
+CREATE TABLE last_6m  (
+  date        date,
+  ticker      varchar(10),
+  name        varchar(255),
+  close       bigint,
+  volume      bigint,
+  insVol      bigint,
+  etcVol      bigint,
+  indVol      bigint,
+  forVol      bigint,
+  insAmt      bigint,
+  etcAmt      bigint,
+  indAmt      bigint,
+  forAmt      bigint,
+  insVolCum   bigint,
+  etcVolCum   bigint,
+  indVolCum   bigint,
+  forVolCum   bigint,
+  insAmtCum   bigint,
+  etcAmtCum   bigint,
+  indAmtCum   bigint,
+  forAmtCum   bigint,
+  primary key (ticker, date)
+) engine = InnoDB;
+
 CREATE TABLE last_12m  (
   date        date,
   ticker      varchar(10),
@@ -63,4 +88,19 @@ CREATE TABLE last_12m  (
   indAmtCum   bigint,
   forAmtCum   bigint,
   primary key (ticker, date)
+) engine = InnoDB;
+
+CREATE TABLE correl(
+  ticker  varchar(10),
+  name    varchar(255),
+  period  varchar(10),
+  insVolCum   double,
+  etcVolCum   double,
+  indVolCum   double,
+  forVolCum   double,
+  insAmtCum   double,
+  etcAmtCum   double,
+  indAmtCum   double,
+  forAmtCum   double,
+  primary key (ticker, period)
 ) engine = InnoDB;
